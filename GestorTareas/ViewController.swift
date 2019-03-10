e12bf0188e8c7df60005f492644fdbea463aa894
Winston Majano <winstonmajano@gmail.com>
2019-03-10 00:17:59 -0600

    Initial Commit

---
+++ GestorTareas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GestorTareas
+//
+//  Created by winston majano on 3/10/19.
+//  Copyright © 2019 whack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
